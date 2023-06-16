c6d4d4896ed18e88a0cb57e3e11fd4d4a51c4a19
Ryan Park <ryrypark@gmail.com>
2023-06-16 12:20:12 -0700

    complete sorting algorithms

--- bubble.js
+++ bubble.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function bubbleSort(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     for (let j = 0; j < i; j++) {
--- merge.js
+++ merge.js
@@ -1,5 +1,44 @@
-function merge() {}
+"use strict";
 
-function mergeSort() {}
+function merge(arr1, arr2) {
+  let result = [];
+  let arr1Idx = 0;
+  let arr2Idx = 0;
+
+  while (arr1Idx < arr1.length && arr2Idx < arr2.length) {
+    if (arr1[arr1Idx] <= arr2[arr2Idx]) {
+      result.push(arr1[arr1Idx]);
+      arr1Idx++;
+    } else {
+      result.push(arr2[arr2Idx]);
+      arr2Idx++;
+    }
+  }
+
+  // arr1Idx === arr1.length ? result.push(...arr2.slice(arr2Idx)) : result.push(...arr1.slice(arr1Idx));
+  // result.push(arr1Idx === arr1.length ? ...arr2.slice(arr2Idx) : ...arr1.slice(arr1Idx));
+
+  if (arr1Idx === arr1.length) {
+    for (let i = arr2Idx; i < arr2.length; i++) {
+      result.push(arr2[i]);
+    }
+  } else {
+    for (let i = arr1Idx; i < arr1.length; i++) {
+      result.push(arr1[i]);
+    }
+  }
+
+  return result
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let half = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, half));
+  let right = mergeSort(arr.slice(half));
+
+  return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
